6fcf836d7654cde55d93d93daef8dae2b453a093
HAJAR OUAKRIM <assma941@gmail.com>
2023-07-26 13:51:29 -0700

    Update Betty.c

--- Betty.c
+++ Betty.c
@@ -1,7 +1,7 @@
 #include "file.h"
 
 /**
- * add - a function that adds two integers.
+ * sum - a function that adds two integers.
  * @a: the first integer.
  * @b: the second integer.
  * Return: Returns the result of the addition.
